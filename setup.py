1e65e1d4e125fab5f7684f8f5888f611488188c0
Daniel Kirov <daniel.kirov@pancentric.com>
2016-05-05 10:22:35 +0100

    Add setup and package-related files.

---
+++ setup.py
@@ -0,0 +1,37 @@
+import os
+from setuptools import setup, find_packages
+
+with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
+    README = readme.read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='django-healthcheck',
+    version='0.1.0',
+    packages=find_packages(),
+    include_package_data=True,
+    license='BSD License',
+    description='Django app to check the health of popular core components of a django-powered webapp',
+    long_description=README,
+    url='https://github.com/DanielKirov/django-healthcheck',
+    author='Daniel Kirov',
+    author_email='daniel.kirov@pancentric.com',
+    install_requires=[
+        'django>=1.4.0',
+        'newrelic>=2.18.1.15'
+    ],
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
+)
